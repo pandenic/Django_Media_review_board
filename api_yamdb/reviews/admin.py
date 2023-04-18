2b9c89d8b838f67d6e23229b74ffe8ffdb5c9883
Denis <114985447+pandenic@users.noreply.github.com>
2023-04-18 23:08:44 +0300

    Добавлена админка и модель User (#2)
    Добавлена админка и модель User

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,24 @@
+"""Модуль содержит настройки для панели администратора приложения reviews."""
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    """Настройки для панели администратора модели User."""
+
+    list_display = (
+        "username",
+        "role",
+        "first_name",
+        "last_name",
+        "email",
+        "bio",
+    )
+    search_fields = ("username", "role")
+    list_filter = ("role",)
+    empty_value_display = "-пусто-"
+    list_editable = ("role",)
+
+
+admin.site.register(User, UserAdmin)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,26 @@
+"""Модуль содержит описание моделей для приложения review."""
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Описание дополнительных полей модели User."""
+
+    ROLES = (
+        ("user", "user"),
+        ("admin", "admin"),
+        ("moderator", "moderator"),
+    )
+
+    bio = models.TextField(
+        verbose_name="Биография",
+        help_text="Укажите биографию пользователей",
+        blank=True,
+    )
+    role = models.CharField(
+        verbose_name="Роль",
+        help_text="Укажите роль пользователя",
+        max_length=9,
+        default="user",
+        choices=ROLES,
+    )
