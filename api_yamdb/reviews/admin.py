9667bf6ebc7236a66d65a5beb11fdfd08a9481f3
elValeron <vbalashov89@gmail.com>
2023-04-19 20:29:56 +0300

    Добавлены модели Review и Comment

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,7 +1,7 @@
 """Модуль содержит настройки для панели администратора приложения reviews."""
 from django.contrib import admin
 from reviews.models import User
-from reviews.models import Category, Genre, Title
+from reviews.models import Comment, Category, Genre, Review, Title
 
 class UserAdmin(admin.ModelAdmin):
     """Настройки для панели администратора модели User."""
@@ -42,6 +42,39 @@ class TitleAdmin(admin.ModelAdmin):
     search_fields = ("name",)
     list_filter = ("year", "genre", "category")
 
+
+class CommenAdmin(admin.ModelAdmin):
+    """
+    Настройки админ-панели для комментариев.
+    """
+    
+    list_display = (
+        'id',
+        'text',
+        'author',
+        'created',
+    )
+    search_fields = ('text', 'author',)
+    list_filter = ('text', 'author', 'created',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    Настройки админ-панели для отзывов.
+    """
+    
+    list_display = (
+        'id',
+        'text',
+        'author',
+        'score',
+        'pub_date',
+    )
+    search_fields = ('text', 'author', 'pub_date',)
+    list_filter = ('text', 'author', 'pub_date',)
+
+admin.site.register(Comment, CommenAdmin)
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre, GenreAdmin)
