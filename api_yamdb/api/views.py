5624615d966541adc95974e43c9e221b8882570d
AI-Stratov <107814128+AI-Stratov@users.noreply.github.com>
2023-04-19 09:36:08 +0300

    Модели, пермишены, валидатор, админка (#4)
    Co-authored-by: Denis <114985447+pandenic@users.noreply.github.com>

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,19 +1,3 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from django_filters.rest_framework import DjangoFilterBackend
 
-from reviews.models import Title
-from api.serializers import TitleViewSerializer
-from api.filters import TitleFilter
-from api.permissions import IsAdminOrReadOnly
-
-
-class TitleViewSet(viewsets.ModelViewSet):
-    """GET-запрос вернет список всех произведений
-    Доступна фильтрация по полям."""
-
-    queryset = Title.objects.all()
-    serializer_class = TitleViewSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filterset_class = TitleFilter
-    permission_classes = (IsAdminOrReadOnly,)
+# Create your views here.
