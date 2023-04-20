9fe8e49c17cf52c58e9b0636438dda7f731ec6ec
elValeron <vbalashov89@gmail.com>
2023-04-20 22:53:22 +0300

    Добавлены вьюсеты для Отзыва и Коммента

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from reviews.models import Review, Comment, Title
 
-# Create your views here.
+from .permissions import IsAuthorOrStaffOrReadOnly
+from .serializers import ReviewSerializer, CommentSerializer, TitleSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    Viewset для просмотра и редактирования Отзывов.
+    """
+
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthorOrStaffOrReadOnly,)
+    
+    def get_title(self):
+        return Title.objects.get(pk=self.kwargs.get("title_id"))
+
+    def get_queryset(self):
+        return self.get_title().reviews.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            title=self.get_title()
+        )
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Viewset для создания и редактирования комментариев.
+    """
+
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrStaffOrReadOnly)
+
+    def get_review(self):
+        return Review.objects.get(pk=self.kwargs.get("review_id"))
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            pk=self.get_review()
+        )
