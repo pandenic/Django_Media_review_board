f2ce26ed568c9cacb005eb321d75ba249d5ef1be
elValeron <vbalashov89@gmail.com>
2023-04-20 22:53:22 +0300

    Добавлены вьюсеты для Отзыва и Коммента

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -14,7 +14,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from api.filters import TitleFilter
 from api.mixins import ListCreateDestroyViewSet
 from api.pagination import UserPagination
-from api.permissions import IsAdminOrReadOnly
+from api.permissions import IsAdminOrReadOnly, IsAuthorOrStaffOrReadOnly
 from api.serializers import (
     CategorySerializer,
     GenreSerializer,
@@ -23,8 +23,10 @@ from api.serializers import (
     UserSerializer,
     GetTokenSerializer,
     SignupSerializer,
+    ReviewSerializer,
+    CommentSerializer
 )
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Genre, Title,  Review
 
 User = get_user_model()
 
@@ -139,3 +141,44 @@ def sign_up(request):
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    Viewset для просмотра и редактирования Отзывов.
+    """
+
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthorOrStaffOrReadOnly,)
+    
+    def get_title(self):
+        return Title.objects.get(pk=self.kwargs.get("title_id"))
+
+    def get_queryset(self):
+        return self.get_title().reviews.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            title=self.get_title()
+        )
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Viewset для создания и редактирования комментариев.
+    """
+
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrStaffOrReadOnly)
+
+    def get_review(self):
+        return Review.objects.get(pk=self.kwargs.get("review_id"))
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            pk=self.get_review()
+        )
