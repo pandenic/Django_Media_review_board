e137a6824e634d4e114ca9b9d5eb42a17dd59e52
Denis <panasenkodf@gmail.com>
2023-04-22 23:55:06 +0400

    Удаление CustomUserRouter и DefaultRouter

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 """Модуль, в котором содержатся url для приложения api."""
 from django.urls import include, path
 
-from rest_framework.routers import DefaultRouter, SimpleRouter
+from rest_framework.routers import SimpleRouter
 
 from api.views import (
     CategoryViewSet,
@@ -10,7 +10,7 @@ from api.views import (
     ReviewViewSet,
     CommentViewSet
     )
-from api.routers import CustomUserRouter
+
 from api.views import UserViewSet, get_token, sign_up
 
 app_name = "api"
@@ -21,36 +21,34 @@ router.register(
     TitleViewSet,
     basename="titles",
 )
-router.register("categories", CategoryViewSet, basename="categories")
+router.register(
+    "categories",
+    CategoryViewSet,
+    basename="categories"
+)
 router.register(
     "genres",
     GenreViewSet,
     basename="genres",
 )
-
-user_router = CustomUserRouter()
-user_router.register(
+router.register(
     "users",
     UserViewSet,
     basename="users",
 )
-
-router_v1 = DefaultRouter()
-router_v1.register(
+router.register(
     r"titles/(?P<title_id>\d+)/reviews",
     ReviewViewSet,
     basename="reviews"
 )
-router_v1.register(
+router.register(
     r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
     CommentViewSet,
     basename="comments"
 )
 
 v1 = [
-    path("", include(router_v1.urls)),
     path("", include(router.urls)),
-    path("", include(user_router.urls)),
     path("auth/token/", get_token, name="token_obtain"),
     path("auth/signup/", sign_up, name="sign_up"),
 ]
