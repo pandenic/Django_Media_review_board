8ab0878ce4a27285a6c82e2a6cbb79edf6688643
Андрей Истратов <workistratov@gmail.com>
2023-04-26 17:25:52 +0300

    Bugfixes in titles

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -2,9 +2,11 @@
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 
+from api.errors import ErrorMessage
+
 
 def validate_year(value):
     """Проверяет если год ещё не наступил."""
     if value > timezone.now().year:
-        raise ValidationError(f"{value} год ещё не наступил")
+        raise ValidationError(f"{value} {ErrorMessage.INVALID_YEAR_ERROR}")
     return value
